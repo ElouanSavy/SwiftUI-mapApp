0000c2d42f68d6b5a7a32eeb94e20df9c45c4dda
Elouan Savy <elouan.savy@gmail.com>
2024-05-05 16:42:51 +0200

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Elouan Savy on 05/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
